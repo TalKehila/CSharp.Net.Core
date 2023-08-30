da663b61913ec68ef2b3b6156e2b371e0bae1fca
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 14:07:49 +0300

    Using in Url and using Page

---
+++ CountriesAPI/CountriesApplicationExample/Controllers/CountriesController.cs
@@ -0,0 +1,58 @@
+﻿using CountriesApplicationExample.Models;
+using CountriesApplicationExample.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+
+namespace CountriesApplicationExample.Controllers
+{
+    public class CountriesController : Controller
+    {   
+        public FetchCountriesService fetch { get; set; }
+        public CountriesController(FetchCountriesService fetch)
+        {
+            this.fetch = fetch;
+        }
+
+        public IActionResult DisplayAllCountries(int pagenumber = 1)
+        {
+            var countries = this.fetch.FetchAllCountries()
+                .Skip(10*(pagenumber - 1))
+                .Take(10)
+            .ToList();
+            return View(countries);
+        }
+
+		public IActionResult DisplayRestrictedCountries(int pagenumber=1)
+        {
+			var countries = this.fetch.FetchResCountries()
+				.Skip(10 * (pagenumber - 1))
+				.Take(10)
+			.ToList();
+			return View(countries);
+		}
+
+        public IActionResult DisplayAllowedCountries( int pagenumber=1)
+        {
+            var countries = this.fetch.FetchAllowedCountries()
+                .Skip(10 * (pagenumber - 1))
+                .Take(10)
+                .ToList();
+            return View(countries);
+        }
+		public IActionResult CheckAllowed(string countryName)
+		{
+			this.fetch.ModifyCountryStatus(countryName, true);
+			return RedirectToAction("DisplayAllowedCountries");
+		}
+
+		public IActionResult CheckNotAllowed(string countryName)
+		{
+			this.fetch.ModifyCountryStatus(countryName, false);
+			return RedirectToAction("DisplayRestrictedCountries");
+		}
+
+
+
+
+	}
+}
---
+++ CountriesAPI/CountriesApplicationExample/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿namespace CountriesApplicationExample.Models
+{
+    public class Country
+    {
+        public CountryName Name{ get; set; }
+        public CountryFlag Flags { get; set; }
+    }
+
+    public class CountryName
+    {
+        public string Common{ get; set; }
+    }
+
+    public class CountryFlag
+    {
+        public string Png{ get; set; }
+    }
+}
